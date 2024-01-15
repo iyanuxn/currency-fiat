699dde644b210066b84c0f3a079e1f392eacc8f4
Iyanu <iyanuxn@gmail.com>
2024-01-15 02:28:50 +0100

    Main setup

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const apiKey =
   "47659afe341aa8d88b5916df968b062d17e0371619c47c3c9aaa2ea69ad7bbcd";
 const coinRankingUrl =
   "https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers%5B0%5D=1&orderBy=marketCap&orderDirection=desc&limit=50&offset=0";
-
 let isConvertingToFiat = true;
 
 function toggleConversion() {
@@ -31,25 +30,43 @@ const coinRankingOptions = {
 };
 
 async function populateCryptoSelect() {
-  const cryptoSelect = document.getElementById("cryptoSelect");
+  const cryptoSelect = document.getElementById("options");
 
   try {
     const response = await fetch(coinRankingUrl, coinRankingOptions);
     const data = await response.json();
 
     data.data.coins.forEach((crypto) => {
-      const option = document.createElement("option");
-      option.value = crypto.symbol;
-      option.textContent = `${crypto.name} (${crypto.symbol})`;
+      const option = document.createElement("div");
+      option.classList.add(
+        "cursor-pointer",
+        "p-2",
+        "transition",
+        "duration-300",
+        "hover:bg-neutral-200",
+        "ease-in-out"
+      );
+      option.textContent = crypto.name;
+      option.addEventListener("click", () => {
+        document.getElementById("cryptoSelect").value = crypto.symbol; // Set the selected value
+        document.getElementById(
+          "cryptoSelectBtn"
+        ).textContent = `${crypto.name}`;
+        toggleCryptoSelect(); // Hide the dropdown after selecting an option
+      });
       cryptoSelect.appendChild(option);
     });
   } catch (error) {
     console.error(error);
-    cryptoSelect.innerHTML = "<option>Error fetching data</option>";
+    cryptoSelect.innerHTML = "<div>Error fetching data</div>";
   }
 }
 
-// Function to populate the target currency dropdown
+document.addEventListener("DOMContentLoaded", () => {
+  populateCryptoSelect();
+  populateTargetCurrencySelect();
+});
+
 async function populateTargetCurrencySelect() {
   const targetCurrencySelect = document.getElementById("targetCurrencySelect");
 
@@ -63,7 +80,22 @@ async function populateTargetCurrencySelect() {
     currencies.forEach((currency) => {
       const option = document.createElement("option");
       option.value = currency;
+      option.classList.add(
+        "cursor-pointer",
+        "p-2",
+        "transition",
+        "duration-300",
+        "hover:bg-neutral-200",
+        "ease-in-out"
+      );
       option.textContent = currency;
+      option.addEventListener("click", () => {
+        document.getElementById("targetCurrencySelect").value = currency; // Set the selected value
+        document.getElementById(
+          "targetCurrencySelectBtn"
+        ).textContent = `${currency}`;
+        toggleTargetCurrencySelect(); // Hide the dropdown after selecting an option
+      });
       targetCurrencySelect.appendChild(option);
     });
   } catch (error) {
@@ -71,11 +103,6 @@ async function populateTargetCurrencySelect() {
     targetCurrencySelect.innerHTML = "<option>Error fetching data</option>";
   }
 }
-
-document.addEventListener("DOMContentLoaded", () => {
-  populateCryptoSelect();
-  populateTargetCurrencySelect();
-});
 
 function performConversion() {
   const selectedCrypto = document.getElementById("cryptoSelect").value;
@@ -103,4 +130,24 @@ function performConversion() {
       console.error(error);
       resultInput.value = "Error fetching data";
     });
+}
+
+function toggleCryptoSelect() {
+  const cryptoSelect = document.getElementById("cryptoSelect");
+
+  if (cryptoSelect.style.display === "none") {
+    cryptoSelect.style.display = "block";
+  } else {
+    cryptoSelect.style.display = "none";
+  }
+}
+
+function toggleTargetCurrencySelect() {
+  const targetCurrencySelect = document.getElementById("targetCurrencySelect");
+
+  if (targetCurrencySelect.style.display === "none") {
+    targetCurrencySelect.style.display = "block";
+  } else {
+    targetCurrencySelect.style.display = "none";
+  }
 }
